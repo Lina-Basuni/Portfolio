0e1ff4d88aa148f5398b1740ecdf814956aaabff
Lina Ahmed Kamal Basuni <lina.ahmedkamal@gmail.com>
2018-09-17 00:06:08 +0200

    main page is complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './lb2.png';
 import './App.css';
-import MainPage from './mainPage'
+import MainPage from './mainPage';
+import NavBar from './navBar'
 
 class App extends Component {
   render() {
@@ -12,7 +13,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
           </div>
           <div className="title-container">
-
+            <NavBar/>
           </div>
         </header>
         <div className="App-intro">
--- src/mainPage.js
+++ src/mainPage.js
@@ -7,6 +7,14 @@ class MainPage extends Component{
 
     return(
       <div className="mainPage">
+       <div className="main-text">
+          <div className="intro-1">
+            Hello, I am Lina Basuni
+          </div>
+          <div className="intro-2">
+            And I'm a Front-End Web Developer
+          </div>
+        </div>
         <div className="dArrow-container">
           <img src={dArrow} className="dArrow" alt="arrow" />
         </div>
